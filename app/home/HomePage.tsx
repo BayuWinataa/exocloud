535fd9185185e29e0418ad08046af900d7634952
dhini <thisisdhinii@gmail.com>
2026-05-08 18:12:09 +0700

    feat: add padding to HomePage layout and update package dependencies for framer-motion

--- app/home/HomePage.tsx
+++ app/home/HomePage.tsx
@@ -3,11 +3,11 @@ import Highlight from "./Highlight";
 import Review from "./Review";
 
 export default function HomePage() {
-  return (
-    <main className="min-h-screen bg-white">
-      <Hero />
-      <Highlight />
-      <Review />
-    </main>
-  );
+	return (
+		<main className="min-h-screen bg-white pt-24">
+			<Hero />
+			<Highlight />
+			<Review />
+		</main>
+	);
 }
